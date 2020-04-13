aa1961d9f0f05948b5dc4ea8f3866992d8053234
André Lins <andrelucas01@hotmail.com>
2020-04-13 10:43:39 -0300

    Responsiveness

--- src/dev/App.svelte
+++ src/dev/App.svelte
@@ -60,6 +60,12 @@
     box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
       0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
   }
+
+  @media screen and (max-width: 800px) {
+    pre {
+      font-size: 15px;
+    }
+  }
 </style>
 
 <InternetConnection let:online>
